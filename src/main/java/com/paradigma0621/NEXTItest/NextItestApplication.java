98d7a5210f95b2424a131b71903bd4a946f671ae
Lucas Favaro Borsatto <paradigma0621@gmail.com>
2021-10-01 08:55:20 -0300

    Initial project using Spring Boot 2.5.5 and MySQL

---
+++ src/main/java/com/paradigma0621/NEXTItest/NextItestApplication.java
@@ -0,0 +1,13 @@
+package com.paradigma0621.NEXTItest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextItestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextItestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paradigma0621/NEXTItest/NextItestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradigma0621.NEXTItest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextItestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
